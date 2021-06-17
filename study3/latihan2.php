ca539685ed2d57e70afd2c2caeb0fd3fa5a5fe90
slackie9 <isminaldiprogram@gmail.com>
2021-06-17 11:00:15 +0700

    study3

---
+++ study3/latihan2.php
@@ -0,0 +1,18 @@
+<?php
+//if else
+// if else if else
+//ternary
+
+//switch
+
+$x = 20;
+if( $x < 20){
+    echo "Right";
+} elseif( $x ==20) {
+    echo"Binggo";
+}
+ else {
+    echo "Wrong";
+}
+
+?>
