32a3313de9d957db364fc6e25a6c82024dea93b9
slackie9 <isminaldiprogram@gmail.com>
2021-06-17 07:12:14 +0700

    add study2

---
+++ study2/index.php
@@ -0,0 +1,34 @@
+<?php
+
+// study 2 - PHP Dasar
+// Sintaks php
+
+// standar Output
+
+echo  "Slackie \r" ;
+echo "test ";
+echo "<br>";
+print "Slackie \n";
+
+print_r("Slackie \n");
+
+var_dump("Slackie");
+// variable
+
+$nama = "slackie variablesss";
+echo "<br>";
+echo " Hallo, nama saya $nama";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test php</title>
+</head>
+<body>
+    <h1>Hello, Welcome <?php echo $nama; ?></h1>
+</body>
+</html>
+
